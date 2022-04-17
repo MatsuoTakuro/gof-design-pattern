83d5b7e0b7323ae9ad9da5936e8a5158058d6b91
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-04-17 16:15:19 +0900

    Add makeLinkPage method

--- src/facade/Main.java
+++ src/facade/Main.java
@@ -5,5 +5,6 @@ import facade.pagemaker.PageMaker;
 public class Main {
     public static void main(String[] args) {
       PageMaker.makeWelcomePage("hyuki@example.com", "welcome.html");
+      PageMaker.makeLinkPage("linkpage.html");
     }
 }
--- src/facade/pagemaker/PageMaker.java
+++ src/facade/pagemaker/PageMaker.java
@@ -5,20 +5,38 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class PageMaker {
+  final static String PATH = "src/facade/";
+
   private PageMaker() {
   }
 
   public static void makeWelcomePage(String mailAddr, String filename) {
     try {
-      Properties mailProp = Database.getProperties("src/facade/maildata");
+      Properties mailProp = Database.getProperties(PATH + "maildata");
       String username = mailProp.getProperty(mailAddr);
-      HtmlWriter writer = new HtmlWriter(new FileWriter("src/facade/"+filename));
+      HtmlWriter writer = new HtmlWriter(new FileWriter(PATH + filename));
       writer.title(username + "'s web page");
       writer.paragraph("Welcome to " + username + "'s web page!");
       writer.paragraph("Nice to meet you!");
       writer.mailTo(mailAddr, username);
       writer.close();
       System.out.println(filename + " is created for " + mailAddr + " (" + username + ")");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void makeLinkPage(String filename) {
+    try {
+      HtmlWriter writer = new HtmlWriter(new FileWriter(PATH + filename));
+      writer.title("Link page");
+      Properties mailProp = Database.getProperties(PATH + "maildata");
+      for (String mailAddr: mailProp.stringPropertyNames()) {
+        String username = mailProp.getProperty(mailAddr, "(unknown)");
+        writer.mailTo(mailAddr, username);
+      }
+      writer.close();
+      System.out.println(filename + "is created.");
     } catch (IOException e) {
       e.printStackTrace();
     }
