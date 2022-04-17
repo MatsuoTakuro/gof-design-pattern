75844de5c2350eee2604fbd56664b18e7111a8a6
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-04-17 16:38:28 +0900

    use Text Blocks

--- src/facade/Main.java
+++ src/facade/Main.java
@@ -6,5 +6,19 @@ public class Main {
     public static void main(String[] args) {
       PageMaker.makeWelcomePage("hyuki@example.com", "welcome.html");
       PageMaker.makeLinkPage("linkpage.html");
-    }
+      String title = "Welcome!";
+      String message = "Hello, world!";
+      String html = """
+      <!DOCTYPE html>
+      <html>
+          <head>
+              <title>%s</title>
+          </head>
+          <body>
+              <h1 style="text-align: center">%s</h1>
+          </body>
+      </html>
+      """.formatted(title, message);
+      System.out.print(html);
+  }
 }
