e271a748273f1257dc7cabf3c35b01cf4f858e3a
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-04-19 00:28:49 +0900

    Limited 3 type Triple instances

---
+++ src/Singleton/Triple.java
@@ -0,0 +1,29 @@
+package Singleton;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Triple {
+    private static Map<String, Triple> map = new HashMap<>();
+    static {
+        String[] names = { "ALPHA", "BETA", "GAMMA" };
+        Arrays.stream(names).forEach(name -> map.put(name, new Triple(name)));
+    }
+
+    private String name;
+
+    private Triple(String name) {
+        System.out.println("The instance " + name + " is created.");
+        this.name = name;
+    }
+
+    public static Triple getInstance(String name) {
+        return map.get(name);
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
