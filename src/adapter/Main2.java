f29c7062b4093ca1b1329407a841406eda5bf51f
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-04-25 11:07:20 +0900

    Print properties

--- src/adapter/Main2.java
+++ src/adapter/Main2.java
@@ -7,9 +7,13 @@ public class Main2 {
         FileIO f = new FileProperties();
         try {
             f.readFromFile("src/adapter/file.txt");
+            System.out.println(f.getValue("width"));
             f.setValue("width", "1024");
+            System.out.println(f.getValue("width"));
             f.setValue("height", "512");
+            System.out.println(f.getValue("height"));
             f.setValue("depth", "32");
+            System.out.println(f.getValue("depth"));
             f.writeToFile("src/adapter/new.text");
             System.out.println("new.text is created.");
         } catch (IOException e) {
