62e1d01af5d868dc0b4607ed043ae8578ee6c992
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-04-30 22:21:11 +0900

    Prototype

---
+++ src/protoype/Main.java
@@ -0,0 +1,26 @@
+package protoype;
+
+import protoype.framework.Manager;
+import protoype.framework.Product;
+
+public class Main {
+    public static void main(String[] args) {
+        Manager manager = new Manager();
+        UnderlinePen uPen = new UnderlinePen('-');
+        MessageBox mBox = new MessageBox('*');
+        MessageBox sBox = new MessageBox('/');
+
+        manager.register("strong message", uPen);
+        manager.register("warning box", mBox);
+        manager.register("slash box", sBox);
+
+        Product p1 = manager.create("strong message");
+        p1.use("Hello, world.");
+
+        Product p2 = manager.create("warning box");
+        p2.use("Hello, world.");
+
+        Product p3 = manager.create("slash box");
+        p3.use("Hello, world.");
+    }
+}
