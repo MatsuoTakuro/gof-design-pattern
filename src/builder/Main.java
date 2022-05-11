faa8ae2d96fcb0f74cc66ada4a9ceaca8e88cde3
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-05-11 10:25:50 +0900

    Add FrameBuilder

--- src/builder/Main.java
+++ src/builder/Main.java
@@ -1,11 +1,14 @@
 package builder;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         if (args.length != 1) {
             usage();
             System.exit(0);
         }
+
         if (args[0].equals("text")) {
             TextBuilder textBuilder = new TextBuilder();
             Director director = new Director(textBuilder);
@@ -18,6 +21,12 @@ public class Main {
             director.construct();
             String filename = htmlBuilder.getHTMLResult();
             System.out.println("html file: " + filename + " was created.");
+        } else if (args[0].equals("frame")) {
+            FrameBuilder frameBuilder = new FrameBuilder();
+            Director director = new Director(frameBuilder);
+            director.construct();
+            JFrame frame = frameBuilder.getFrameResult();
+            frame.setVisible(true);
         } else {
             usage();
             System.exit(0);
