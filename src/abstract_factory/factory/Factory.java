76a3b5df81b9608c6895b397e9f8c01dfe912567
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-05-13 13:58:36 +0900

    Make Yahoo page

--- src/abstract_factory/factory/Factory.java
+++ src/abstract_factory/factory/Factory.java
@@ -16,4 +16,10 @@ public abstract class Factory {
     public abstract Link createLink(String caption, String url);
     public abstract Tray createTray(String caption);
     public abstract Page createPage(String title, String author);
+    public Page createYahooPage() {
+        Link link = createLink("Yahoo!", "https://www.yahoo.com/");
+        Page page = createPage("Yahoo!", "Yahoo!");
+        page.add(link);
+        return page;
+    }
 }
